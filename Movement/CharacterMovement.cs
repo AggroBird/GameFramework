7fecb451fa9aeff07913caec92d5642f4a81f925
AggroBird <jan@aggrobird.com>
2023-06-08 13:49:34 +0900

    Stripped unused code

--- Movement/CharacterMovement.cs
+++ Movement/CharacterMovement.cs
@@ -6,14 +6,18 @@ namespace AggroBird.GameFramework
     // Based on physics based character controller by Toyful games https://www.toyfulgames.com/
     public class CharacterMovement : Movement
     {
+        private enum LookDirectionOptions { Velocity, Acceleration, Input };
+
         [SerializeField, HideInInspector] private new Rigidbody rigidbody;
         [SerializeField, HideInInspector] private new CapsuleCollider collider;
         private Vector3 _gravitationalForce;
         private Vector3 _previousVelocity = Vector3.zero;
+        private static PhysicMaterial physicMaterial = default;
 
         [Header("Other")]
-        [SerializeField] private bool _adjustInputsToCameraAngle = false;
         [SerializeField] private LayerMask _terrainLayer;
+        [SerializeField] private LookDirectionOptions _characterLookDirection = LookDirectionOptions.Velocity;
+        [SerializeField] private float _rotateSpeed = 360;
 
         private bool _shouldMaintainHeight = true;
 
@@ -24,33 +28,18 @@ namespace AggroBird.GameFramework
         [SerializeField] private float _rideSpringDamper = 10; // rideSpringDampener: dampener of spring. (?)
 
 
-        private enum LookDirectionOptions { Velocity, Acceleration, Input };
-        private Quaternion _uprightTargetRot = Quaternion.identity; // Adjust y value to match the desired direction to face.
-        private Quaternion _lastTargetRot;
-        private Vector3 _platformInitRot;
-        private bool didLastRayHit;
-
-        [Header("Upright Spring")]
-        [SerializeField] private LookDirectionOptions _characterLookDirection = LookDirectionOptions.Velocity;
-        [SerializeField] private float _uprightSpringStrength = 40;
-        [SerializeField] private float _uprightSpringDamper = 5;
-
-
         private float _speedFactor = 1f;
         private float _maxAccelForceFactor = 1f;
         private Vector3 _m_GoalVel = Vector3.zero;
 
         [Header("Movement")]
-        [SerializeField] private float _maxSpeed = 8f;
-        [SerializeField] private float _acceleration = 200f;
-        [SerializeField] private float _maxAccelForce = 150f;
-        [SerializeField] private float _leanFactor = 0.25f;
+        [SerializeField] private float _maxSpeed = 10;
+        [SerializeField] private float _acceleration = 400;
+        [SerializeField] private float _maxAccelForce = 300;
         [SerializeField] private AnimationCurve _accelerationFactorFromDot;
         [SerializeField] private AnimationCurve _maxAccelerationForceFactorFromDot;
-        [SerializeField] private Vector3 _moveForceScale = new Vector3(1f, 0f, 1f);
 
-
-        private Vector3 _jumpInput;
+        private bool jumpInput = false;
         private float _timeSinceJumpPressed = 0f;
         private float _timeSinceUngrounded = 0f;
         private float _timeSinceJump = 0f;
@@ -58,6 +47,7 @@ namespace AggroBird.GameFramework
         private bool _isJumping = false;
 
         [Header("Jump")]
+        [SerializeField] private bool _canJump = true;
         [SerializeField] private float _jumpForceFactor = 10f;
         [SerializeField] private float _riseGravityFactor = 5f;
         [SerializeField] private float _fallGravityFactor = 10f; // typically > 1f (i.e. 5f).
@@ -65,9 +55,8 @@ namespace AggroBird.GameFramework
         [SerializeField] private float _jumpBuffer = 0.15f; // Note, jumpBuffer shouldn't really exceed the time of the jump.
         [SerializeField] private float _coyoteTime = 0.25f;
 
-        private static PhysicMaterial physicMaterial = default;
-
         [System.NonSerialized] public Vector2 movementInput;
+
 
 
         private void Awake()
@@ -78,7 +67,7 @@ namespace AggroBird.GameFramework
             if (!physicMaterial)
             {
                 physicMaterial = new PhysicMaterial();
-                physicMaterial.dynamicFriction = physicMaterial.staticFriction = 0.6f;
+                physicMaterial.dynamicFriction = physicMaterial.staticFriction = 0;
                 physicMaterial.bounciness = 0;
                 physicMaterial.frictionCombine = PhysicMaterialCombine.Average;
                 physicMaterial.bounceCombine = PhysicMaterialCombine.Average;
@@ -106,39 +95,37 @@ namespace AggroBird.GameFramework
             return grounded;
         }
 
-        /// <summary>
-        /// Gets the look desired direction for the character to look.
-        /// The method for determining the look direction is depends on the lookDirectionOption.
-        /// </summary>
-        /// <param name="lookDirectionOption">The factor which determines the look direction: velocity, acceleration or moveInput.</param>
-        /// <returns>The desired look direction.</returns>
-        private Vector3 GetLookDirection(LookDirectionOptions lookDirectionOption)
+        private void UpdateLookDirection()
         {
-            Vector3 lookDirection = Vector3.zero;
-            if (lookDirectionOption == LookDirectionOptions.Velocity || lookDirectionOption == LookDirectionOptions.Acceleration)
+            float target;
+            if (_characterLookDirection == LookDirectionOptions.Velocity || _characterLookDirection == LookDirectionOptions.Acceleration)
             {
                 Vector3 velocity = rigidbody.velocity;
                 velocity.y = 0f;
-                if (lookDirectionOption == LookDirectionOptions.Velocity)
+                if (_characterLookDirection == LookDirectionOptions.Velocity)
                 {
-                    lookDirection = velocity;
+                    target = Mathfx.AngleFromVectorDeg(velocity);
                 }
-                else if (lookDirectionOption == LookDirectionOptions.Acceleration)
+                else
                 {
                     Vector3 deltaVelocity = velocity - _previousVelocity;
                     _previousVelocity = velocity;
                     Vector3 acceleration = deltaVelocity / Time.fixedDeltaTime;
-                    lookDirection = acceleration;
+                    target = Mathfx.AngleFromVectorDeg(acceleration);
                 }
             }
-            else if (lookDirectionOption == LookDirectionOptions.Input)
+            else if (movementInput.sqrMagnitude > 0)
             {
-                lookDirection = movementInput.Horizontal3D();
+                target = Mathfx.AngleFromVectorDeg(movementInput);
             }
-            return lookDirection;
+            else
+            {
+                return;
+            }
+
+            transform.SetYaw(Mathf.MoveTowardsAngle(transform.GetYaw(), target, _rotateSpeed * Time.fixedDeltaTime));
         }
 
-        private bool _prevGrounded = false;
         /// <summary>
         /// Determines and plays the appropriate character sounds, particle effects, then calls the appropriate methods to move and float the character.
         /// </summary>
@@ -152,14 +139,7 @@ namespace AggroBird.GameFramework
                 input /= len;
             }
 
-            if (_adjustInputsToCameraAngle)
-            {
-                //_moveInput = AdjustInputToFaceCamera(_moveInput);
-            }
-
             bool rayHitGround = RaycastToGround(out RaycastHit hit);
-            SetPlatform(hit);
-
             bool grounded = CheckIfGrounded(rayHitGround, hit);
             if (grounded == true)
             {
@@ -175,18 +155,15 @@ namespace AggroBird.GameFramework
                 _timeSinceUngrounded += Time.fixedDeltaTime;
             }
 
-            CharacterMove(input, hit);
-            CharacterJump(_jumpInput, grounded, hit);
+            CharacterMove(input);
+            CharacterJump(jumpInput, grounded, hit);
 
             if (rayHitGround && _shouldMaintainHeight)
             {
                 MaintainHeight(hit);
             }
 
-            Vector3 lookDirection = GetLookDirection(_characterLookDirection);
-            MaintainUpright(lookDirection, hit);
-
-            _prevGrounded = grounded;
+            UpdateLookDirection();
         }
 
         /// <summary>
@@ -220,157 +197,16 @@ namespace AggroBird.GameFramework
             float currHeight = rayHit.distance - _rideHeight;
             float springForce = (currHeight * _rideSpringStrength) - (relVel * _rideSpringDamper);
             Vector3 maintainHeightForce = -_gravitationalForce + springForce * Vector3.down;
-            Vector3 oscillationForce = springForce * Vector3.down;
             rigidbody.AddForce(maintainHeightForce);
-            //Debug.DrawLine(transform.position, transform.position + (_rayDir * springForce), Color.yellow);
-
-            // Apply force to objects beneath
-            if (hitBody != null)
-            {
-                hitBody.AddForceAtPosition(-maintainHeightForce, rayHit.point);
-            }
         }
-
-        /// <summary>
-        /// Determines the desired y rotation for the character, with account for platform rotation.
-        /// </summary>
-        /// <param name="yLookAt">The input look rotation.</param>
-        /// <param name="rayHit">The rayHit towards the platform.</param>
-        private void CalculateTargetRotation(Vector3 yLookAt, RaycastHit rayHit = new RaycastHit())
-        {
-            if (didLastRayHit)
-            {
-                _lastTargetRot = _uprightTargetRot;
-                try
-                {
-                    _platformInitRot = transform.parent.rotation.eulerAngles;
-                }
-                catch
-                {
-                    _platformInitRot = Vector3.zero;
-                }
-            }
-            if (rayHit.rigidbody == null)
-            {
-                didLastRayHit = true;
-            }
-            else
-            {
-                didLastRayHit = false;
-            }
-
-            if (yLookAt != Vector3.zero)
-            {
-                _uprightTargetRot = Quaternion.LookRotation(yLookAt, Vector3.up);
-                _lastTargetRot = _uprightTargetRot;
-                try
-                {
-                    _platformInitRot = transform.parent.rotation.eulerAngles;
-                }
-                catch
-                {
-                    _platformInitRot = Vector3.zero;
-                }
-            }
-            else
-            {
-                try
-                {
-                    Vector3 platformRot = transform.parent.rotation.eulerAngles;
-                    Vector3 deltaPlatformRot = platformRot - _platformInitRot;
-                    float yAngle = _lastTargetRot.eulerAngles.y + deltaPlatformRot.y;
-                    _uprightTargetRot = Quaternion.Euler(new Vector3(0f, yAngle, 0f));
-                }
-                catch
-                {
-
-                }
-            }
-        }
-
-        /// <summary>
-        /// Adds torque to the character to keep the character upright, acting as a torsional oscillator (i.e. vertically flipped pendulum).
-        /// </summary>
-        /// <param name="yLookAt">The input look rotation.</param>
-        /// <param name="rayHit">The rayHit towards the platform.</param>
-        private void MaintainUpright(Vector3 yLookAt, RaycastHit rayHit = new RaycastHit())
-        {
-            CalculateTargetRotation(yLookAt, rayHit);
-
-            Quaternion currentRot = transform.rotation;
-            Quaternion toGoal = ShortestRotation(_uprightTargetRot, currentRot);
-
-            Vector3 rotAxis;
-            float rotDegrees;
-
-            toGoal.ToAngleAxis(out rotDegrees, out rotAxis);
-            rotAxis.Normalize();
-
-            float rotRadians = rotDegrees * Mathf.Deg2Rad;
-
-            rigidbody.AddTorque((rotAxis * (rotRadians * _uprightSpringStrength)) - (rigidbody.angularVelocity * _uprightSpringDamper));
-        }
-        private static Quaternion ShortestRotation(Quaternion a, Quaternion b)
-        {
-            if (Quaternion.Dot(a, b) < 0)
-            {
-                return a * Quaternion.Inverse(Multiply(b, -1));
-            }
-
-            else return a * Quaternion.Inverse(b);
-        }
-        private static Quaternion Multiply(Quaternion input, float scalar)
-        {
-            return new Quaternion(input.x * scalar, input.y * scalar, input.z * scalar, input.w * scalar);
-        }
-
-        /*/// <summary>
-        /// Reads the player movement input.
-        /// </summary>
-        /// <param name="context">The move input's context.</param>
-        public void MoveInputAction(InputAction.CallbackContext context)
-        {
-            _moveContext = context.ReadValue<Vector2>();
-        }
-
-        /// <summary>
-        /// Reads the player jump input.
-        /// </summary>
-        /// <param name="context">The jump input's context.</param>
-        public void JumpInputAction(InputAction.CallbackContext context)
-        {
-            float jumpContext = context.ReadValue<float>();
-            _jumpInput = new Vector3(0, jumpContext, 0);
-
-            if (context.started) // button down
-            {
-                _timeSinceJumpPressed = 0f;
-            }
-        }*/
 
         public void Jump()
         {
-            _jumpInput = new Vector3(0, 1, 0);
-            _timeSinceJumpPressed = 0f;
-        }
-
-        /// <summary>
-        /// Set the transform parent to be the result of RaycastToGround.
-        /// If the raycast didn't hit, then unset the transform parent.
-        /// </summary>
-        /// <param name="rayHit">The rayHit towards the platform.</param>
-        private void SetPlatform(RaycastHit rayHit)
-        {
-            //try
-            //{
-            //    RigidPlatform rigidPlatform = rayHit.transform.GetComponent<RigidPlatform>();
-            //    RigidParent rigidParent = rigidPlatform.rigidParent;
-            //    transform.SetParent(rigidParent.transform);
-            //}
-            //catch
-            //{
-            //    transform.SetParent(null);
-            //}
+            if (_canJump)
+            {
+                jumpInput = true;
+                _timeSinceJumpPressed = 0f;
+            }
         }
 
         /// <summary>
@@ -378,22 +214,18 @@ namespace AggroBird.GameFramework
         /// </summary>
         /// <param name="moveInput">The player movement input.</param>
         /// <param name="rayHit">The rayHit towards the platform.</param>
-        private void CharacterMove(Vector2 moveInput, RaycastHit rayHit)
+        private void CharacterMove(Vector2 moveInput)
         {
             Vector3 m_UnitGoal = moveInput.Horizontal3D();
             Vector3 unitVel = _m_GoalVel.normalized;
             float velDot = Vector3.Dot(m_UnitGoal, unitVel);
             float accel = _acceleration * _accelerationFactorFromDot.Evaluate(velDot);
             Vector3 goalVel = m_UnitGoal * _maxSpeed * _speedFactor;
-            Vector3 otherVel = Vector3.zero;
-            Rigidbody hitBody = rayHit.rigidbody;
-            _m_GoalVel = Vector3.MoveTowards(_m_GoalVel,
-                                            goalVel,
-                                            accel * Time.fixedDeltaTime);
+            _m_GoalVel = Vector3.MoveTowards(_m_GoalVel, goalVel, accel * Time.fixedDeltaTime);
             Vector3 neededAccel = (_m_GoalVel - rigidbody.velocity) / Time.fixedDeltaTime;
             float maxAccel = _maxAccelForce * _maxAccelerationForceFactorFromDot.Evaluate(velDot) * _maxAccelForceFactor;
             neededAccel = Vector3.ClampMagnitude(neededAccel, maxAccel);
-            rigidbody.AddForceAtPosition(Vector3.Scale(neededAccel * rigidbody.mass, _moveForceScale), transform.position + new Vector3(0f, transform.localScale.y * _leanFactor, 0f)); // Using AddForceAtPosition in order to both move the player and cause the play to lean in the direction of input.
+            rigidbody.AddForceAtPosition(Vector3.Scale(neededAccel * rigidbody.mass, new Vector3(1, 0, 1)), transform.position);
         }
 
         /// <summary>
@@ -402,7 +234,7 @@ namespace AggroBird.GameFramework
         /// <param name="jumpInput">The player jump input.</param>
         /// <param name="grounded">Whether or not the player is considered grounded.</param>
         /// <param name="rayHit">The rayHit towards the platform.</param>
-        private void CharacterJump(Vector3 jumpInput, bool grounded, RaycastHit rayHit)
+        private void CharacterJump(bool jumpInput, bool grounded, RaycastHit rayHit)
         {
             _timeSinceJumpPressed += Time.fixedDeltaTime;
             _timeSinceJump += Time.fixedDeltaTime;
@@ -424,7 +256,7 @@ namespace AggroBird.GameFramework
                     {
                         rigidbody.AddForce(_gravitationalForce * (_riseGravityFactor - 1f));
                     }
-                    if (jumpInput == Vector3.zero)
+                    if (!jumpInput)
                     {
                         // Impede the jump height to achieve a low jump.
                         rigidbody.AddForce(_gravitationalForce * (_lowJumpFactor - 1f));
@@ -452,9 +284,19 @@ namespace AggroBird.GameFramework
         }
 
 #if UNITY_EDITOR
+        protected virtual void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(transform.position, transform.position - new Vector3(0, _rideHeight, 0));
+        }
         protected virtual void OnValidate()
         {
-            Utility.EnsureComponentReference(this, ref rigidbody);
+            if (_rideHeight > _rayToGroundLength) _rayToGroundLength = _rideHeight;
+
+            if (Utility.EnsureComponentReference(this, ref rigidbody))
+            {
+
+            }
             Utility.EnsureComponentReference(this, ref collider);
         }
 #endif
